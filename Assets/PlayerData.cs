c73d25caa192c4ffc70777aa9f9a86ec32415816
Y-L77 <streamluu@gmail.com>
2024-11-24 10:17:35 -0500

    fixed chiranjiv's bugs

--- Assets/PlayerData.cs
+++ Assets/PlayerData.cs
@@ -86,27 +86,21 @@ public class PlayerData : MonoBehaviour
         if (touchingChicken && Input.GetKey(KeyCode.E))
         {
             chickenObject.SetActive(true);
-<<<<<<< HEAD
             holdingChicken = true; // Set to false when used in future logic
-=======
             pickUpChicken.Play();
             holdingChicken = true; // later when I add campfire logic, set both false
->>>>>>> soundeffects
         }
 
         if (holdingChicken && touchingFire && Input.GetKey(KeyCode.E))
         {
-<<<<<<< HEAD
             holdingChicken = false;
             chickenObject.SetActive(false);
-=======
             if (Input.GetKey(KeyCode.E))
             {
                 holdingChicken = false;
                 chickenObject.SetActive(false);
                 cookChicken.Play();
             }
->>>>>>> soundeffects
         }
 
         if (touchingChickenLeg && Input.GetKey(KeyCode.E))
